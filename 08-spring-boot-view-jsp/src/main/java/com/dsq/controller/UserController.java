43899ebafcc5aee07658ebac20eac410cded8816
sdong3 <shiqing.dong@dxc.com>
2019-05-07 15:27:41 +0800

    one commit

---
+++ 08-spring-boot-view-jsp/src/main/java/com/dsq/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.dsq.controller;
+
+import com.dsq.pojo.Users;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/showUser")
+    public String showUser(Model model){
+        List<Users> list = new ArrayList<>();
+        list.add(new Users(1,"aa",10));
+        list.add(new Users(2,"bb",11));
+        list.add(new Users(3,"cc",12));
+
+        model.addAttribute("list", list);
+
+        return "userList";
+    }
+}
